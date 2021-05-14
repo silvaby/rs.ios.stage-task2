ebb547e42d28ddd00ee12664c6000533e0c573d1
Dzmitry <skysilvaby@gmail.com>
2021-05-14 14:53:04 +0300

    feat: exercise-1 done

--- Objc_Task2/ios.stage-task/Exercises/Exercise-1/ReverseBinary.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-1/ReverseBinary.m
@@ -1,5 +1,13 @@
 #import "ReverseBinary.h"
 
 UInt8 ReverseInteger(UInt8 n) {
-    return 0;
+    UInt8 result = 0;
+    
+    for (int i = 0; i < 8; i++) {
+        if ( n % 2) {
+            result += pow(2, 7 - i);
+        }
+        n /= 2;
+    }
+    return result;
 }
